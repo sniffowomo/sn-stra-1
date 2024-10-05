949946a23d21228a7c17dddc57993d06976583e8
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-10-05 20:05:25 +0000

    🫦

--- strato/V1/ar1.sh
+++ strato/V1/ar1.sh
@@ -6,6 +6,10 @@ ar1() {
     echo "-- Runnign Commands here---"
     artem \
         $FIL \
-        --ratio 0.2
+        --ratio 0.4 \
+        -s 30 \
+        --invert \
+        -c s,t,r,a,t,o \
+        -o l1.html
 }
 ar1
