a7abd8f72803f396038908a97ed23f2e1e94e10e
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-10-05 18:45:49 +0000

    🫦

--- wo4/m1.sh
+++ wo4/m1.sh
@@ -4,22 +4,23 @@
 
 TH="abap algol algol_nu arduino autumn average base16-snazzy borland bw charm colorful doom-one doom-one2 dracula emacs friendly fruity github github-dark gruvbox gruvbox-light hr_high_contrast hrdark igor lovelace manni modus-operandi modus-vivendi monokai monokailight murphy native nord onedark onesenterprise praiso-dark praiso-light pastie perldoc pygments rainbow_dash rose-pine rose-pine-dawn rose-pine-moon rrt solarized-dark solarized-dark256 solarized-light swapoff tango trac vim vs vulcan witchhazel xcode xcode-dark"
 LA="javascript ml rust docker dockerfile html css"
-f2() {
+f3() {
     # Header
     echo "f2-CheckLangswithColorfulTheme"
 
     # settings
     fi="m2.sh"
     th1="colorful"
+    lan="javascript"
 
     # Actual command
-    for lang in $LA; do
+    for theme in $TH; do
         freeze "$fi" \
-            -t $th1 \
-            -l $lang \
-            -o pix/f2$lang.png \
+            -t $theme \
+            -l $lan \
+            -o pix/f3$theme.png \
             --show-line-numbers \
             --window
     done
 }
-f2
+f3
