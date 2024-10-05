392545a664c39bb6972d6d583cd6741535ca7b6c
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-10-05 18:28:13 +0000

    🫦

--- wo3/m1.sh
+++ wo3/m1.sh
@@ -3,21 +3,23 @@
 # set themes variable
 
 TH="abap algol algol_nu arduino autumn average base16-snazzy borland bw charm colorful doom-one doom-one2 dracula emacs friendly fruity github github-dark gruvbox gruvbox-light hr_high_contrast hrdark igor lovelace manni modus-operandi modus-vivendi monokai monokailight murphy native nord onedark onesenterprise praiso-dark praiso-light pastie perldoc pygments rainbow_dash rose-pine rose-pine-dawn rose-pine-moon rrt solarized-dark solarized-dark256 solarized-light swapoff tango trac vim vs vulcan witchhazel xcode xcode-dark"
-
-f1() {
+LA="javascript ml rust docker dockerfile html css"
+f2() {
     # Header
-    echo "IPS-in all themses"
+    echo "f2-CheckLangswithColorfulTheme"
 
     # settings
-    fi="ips.txt"
+    fi="m2.sh"
+    th1="colorful"
 
     # Actual command
-    for theme in $TH; do
+    for lang in $LA; do
         freeze "$fi" \
-            -t $theme \
-            -o pix/f1$theme.png \
+            -t $th1 \
+            -l $lang \
+            -o pix/f2$lang.png \
             --show-line-numbers \
             --window
     done
 }
-f1
+f2
